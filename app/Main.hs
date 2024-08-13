7354d6d98ae35de47fdd27e9bac21d4267101feb
Thiago Fernandes Dias <thiago.fernandesdias235@gmail.com>
2024-08-13 18:03:24 -0300

    Print do comando "tesseract <img> stdout".

--- app/Main.hs
+++ app/Main.hs
@@ -1,62 +1,65 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# language ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main (main) where
 
-import Web.Scotty
-
 import Control.Exception (SomeException)
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy as B
 import Data.Foldable (for_)
 import qualified Data.Text.Lazy as TL
-
 import Network.Wai.Middleware.RequestLogger
 import Network.Wai.Middleware.Static
-import Network.Wai.Parse (fileName, fileContent, defaultParseRequestBodyOptions)
-
+import Network.Wai.Parse (defaultParseRequestBodyOptions, fileContent, fileName)
+import System.FilePath ((</>))
+import System.Process
+import Text.Blaze.Html.Renderer.Text (renderHtml)
 import qualified Text.Blaze.Html5 as H
 import Text.Blaze.Html5.Attributes
-import Text.Blaze.Html.Renderer.Text (renderHtml)
+import Web.Scotty
 
-import qualified Data.ByteString.Lazy as B
-import qualified Data.ByteString.Char8 as BS
-import System.FilePath ((</>))
-
-{-| NB : the file paths where files are saved and looked up are relative, so make sure
-to run this program from the root directory of the 'scotty' repo, or adjust the paths
-accordingly.
--}
-
+-- | NB : the file paths where files are saved and looked up are relative, so make sure
+-- to run this program from the root directory of the 'scotty' repo, or adjust the paths
+-- accordingly.
 main :: IO ()
 main = scotty 3000 $ do
-    middleware logStdoutDev
-    middleware $ staticPolicy (noDots >-> addBase "examples/uploads")
+  middleware logStdoutDev
+  middleware $ staticPolicy (noDots >-> addBase "examples/uploads")
 
-    get "/" $ do
-        html $ renderHtml
-             $ H.html $ do
-                H.body $ do
-                    H.form H.! method "post" H.! enctype "multipart/form-data" H.! action "/upload" $ do
-                        H.input H.! type_ "file" H.! name "file_1"
-                        H.br
-                        H.input H.! type_ "file" H.! name "file_2"
-                        H.br
-                        H.input H.! type_ "submit"
+  get "/" $ do
+    html $
+      renderHtml $
+        H.html $ do
+          H.body $ do
+            H.form H.! method "post" H.! enctype "multipart/form-data" H.! action "/upload" $ do
+              H.input H.! type_ "file" H.! name "file_1"
+              H.br
+              H.input H.! type_ "file" H.! name "file_2"
+              H.br
+              H.input H.! type_ "submit"
 
-    post "/upload" $ do
-      filesOpts defaultParseRequestBodyOptions $ \_ fs -> do
-        let
-          fs' = [(fieldName, BS.unpack (fileName fi), fileContent fi) | (fieldName, fi) <- fs]
-        -- write the files to disk, so they can be served by the static middleware
-        for_ fs' $ \(_, fnam, fpath) -> do
-          -- copy temp file to local dir
-          liftIO (do
-                     fc <- B.readFile fpath
-                     B.writeFile ("examples" </> "uploads" </> fnam) fc
-                 ) `catch` (\(e :: SomeException) -> do
-                               liftIO $ putStrLn $ unwords ["upload: something went wrong while saving temp files :", show e]
-                           )
-          -- generate list of links to the files just uploaded
-          html $ mconcat [ mconcat [ TL.fromStrict fName
-                                   , ": "
-                                   , renderHtml $ H.a (H.toHtml fn) H.! (href $ H.toValue fn) >> H.br
-                                   ]
-                         | (fName,fn,_) <- fs' ]
+  post "/upload" $ do
+    filesOpts defaultParseRequestBodyOptions $ \_ fs -> do
+      let fs' = [(fieldName, BS.unpack (fileName fi), fileContent fi) | (fieldName, fi) <- fs]
+      -- write the files to disk, so they can be served by the static middleware
+      for_ fs' $ \(_, fnam, fpath) -> do
+        liftIO
+          ( do
+              output <- readProcess "tesseract" [fpath, "stdout"] ""
+              putStrLn output
+              -- fc <- B.readFile fpath
+              -- B.writeFile ("./examples" </> "uploads" </> fnam) fc
+          )
+          `catch` ( \(e :: SomeException) -> do
+                      liftIO $ putStrLn $ unwords ["upload: something went wrong while saving temp files :", show e]
+                  )
+        -- generate list of links to the files just uploaded
+        html $
+          mconcat
+            [ mconcat
+                [ TL.fromStrict fName,
+                  ": ",
+                  renderHtml $ H.a (H.toHtml fn) H.! (href $ H.toValue fn) >> H.br
+                ]
+              | (fName, fn, _) <- fs'
+            ]
