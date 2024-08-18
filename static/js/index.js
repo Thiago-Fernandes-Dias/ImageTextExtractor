94e39208019f85ae5ec7b58eaf0225c738e80df3
William Fernandes Dias <william.winchester1967@gmail.com>
2024-08-18 17:01:06 -0300

    fix: correctly add the drop classes and prevent browser from opening the dragged files

--- static/js/index.js
+++ static/js/index.js
@@ -2,19 +2,30 @@ let dropArea = document.getElementById('drop-area');
 let imageInput = document.getElementById('image-input');
 let extractForm = document.getElementById('extract-form');
 
-const dragClasses = ['border-gray-700', 'border-4', 'bg-gray-200'];
+const dragClasses = ['border-gray-500', 'border-2', 'bg-gray-100'];
 
-dropArea.addEventListener('dragover', function () {
-	this.classList.add(dragClasses);
+dropArea.addEventListener('dragover', function (e) {
+	dragClasses.forEach(className => {
+		this.classList.add(className);
+	});
+	e.preventDefault();
 });
 
-dropArea.addEventListener('dragleave', function () {
-	this.classList.remove(dragClasses);
+dropArea.addEventListener('dragleave', function (e) {
+	dragClasses.forEach(className => {
+		this.classList.remove(className);
+	});
+	e.preventDefault();
 });
 
 dropArea.addEventListener('drop', function (e) {
-	e.preventDefault();
-	this.classList.remove(dragClasses);
+	dragClasses.forEach(className => {
+		this.classList.remove(className);
+	});
+
 	imageInput.files = e.dataTransfer.files;
 	extractForm.submit();
+
+	e.preventDefault();
+	e.stopPropagation();
 });
